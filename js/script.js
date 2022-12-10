feb0ecb3e2e9801652a436cb9f4085e19aeec036
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2022-12-10 09:13:23 +0100

    Creati due aray

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-  function createBombsArray(min, max){
-      let bombs=[];
+function createBombsArray(min, max){
+    let bombs=[];
       let i=0;
 
       while( i < 16){
@@ -13,7 +13,7 @@
   return bombs
 }
 
- 
+
 let button = document.getElementById('item-up');
 
 button.addEventListener('click', function(){ 
@@ -27,45 +27,42 @@ if(grid){
 }
 
 // Recupero valore livello
-grid_number = document.getElementById('level').value;
+grid_number = parseInt(document.getElementById('level').value);
+
+// creati 2 arrayBombs, 1 per i numeri 1 per le bombe
+
+let arrayBombs =[];
+arrayBombs = createBombsArray(1, grid_number) 
+console.log(arrayBombs)
+
+let arrayNumb = [];
+let j=1;
+while (j < (grid_number + 1 )){
+    arrayNumb.push(j);
+    j++
+
+}
+console.log(arrayNumb)
+if(arrayBombs == arrayNumb){
+    alert ('ciao')
+}
 
 // Ciclo per generare i quadrati con richiamo alla funzione
 for(let i = 0; i < grid_number; i++){
     const currentSquare = createGridSquare(i+1);
     grid.appendChild(currentSquare);
+
+
 // Click sui quadrati
-    currentSquare.addEventListener('click', function(){
-        this.classList.toggle('clicked')
-        console.log(`La casella cliccata è la numero: ${this.innerText}`)
-    })
-    
-    
+currentSquare.addEventListener('click', function(){
+    this.classList.toggle('clicked')
+    console.log(`La casella cliccata è la numero: ${this.innerText}`)
+},{once:`true`})
 }
 
 
+
 // array bombe
-let arrayBombs =[];
-arrayBombs = createBombsArray(1, grid_number)
-console.log(arrayBombs)
-
-
-
-// ciclo tasti rossi
-// for(let i = 0; i < grid_number; i++){
-//     const arrayBombs = createBombsArray(1, grid_number)
-//     grid.appendChild(arrayBombs);
-// // Click sui quadrati
-//     arrayBombs.addEventListener('click', function(){
-//         this.classList.toggle('clicked')
-//         this.classList.add('red')
-//         grid.classList.add('remove')
-//         console.log(`La casella cliccata è la numero: ${this.innerText}`)
-//     })
-    
-    
-// }
-
-
 
 function createGridSquare(number){
     if(grid_number == 100){
@@ -73,16 +70,17 @@ function createGridSquare(number){
         currentElement.classList.add("square");
         currentElement.innerText = number;
         return currentElement;
+   
     }
-    else if(grid_number == 81){
 
+    else if(grid_number == 81){
         const currentElement = document.createElement('div');
         currentElement.classList.add("square_med");
         currentElement.innerText = number;
         return currentElement;
     }
-    else{
 
+    else{
         const currentElement = document.createElement('div');
         currentElement.classList.add("square_min");
         currentElement.innerText = number;
@@ -91,63 +89,3 @@ function createGridSquare(number){
 }
 })
 
-
-
-// funzione che genera numeri casuali
-// Numero compreso tra 1 e numero di celle
-// non ci sono due numeri uguali
-// creiamo un'array vuoto per mettere i numeri gia usciti
-// arrayBombs = createBombsArray(cellsNumber)
-// console.log(arrayBombs)
-
-// let cellsNumber
-
-
-//  function createBombsArray(min, max){
-//      let bombs=[];
-//      let i=0;
-//      while(i < 16){
-//      let number = Math.floor(Math.random() * (max - min + 1)) + min;
-//      if(!arrayBombs.includes(number)){
-//          arrayBombs.push(number);
-//          i++;
-//      }
-//  }
-
-//  console.log(bombs)
-//  return bombs;
-//  }
-
-
-
-
-
-
-//  function createBombsArray(number_cells){
-//      let bombs = [];
-//      for(let i = 0; i < 16; i++){
-//          let random_number = generateUniqueRandomNumbers(bombs, 1, number_cells)
-//          bombs.push(random_number)
-//         }
-//         console.log(bombs)
-//      return bombs;
-//  }
-
-//  function generateUniqueRandomNumbers(bombs, min, max)
-//  {
-//      let flag = false;
-//      let randomInt;
-//      while(flag == false){
-//          randomInt = Math.floor(Math.random() * (max - min + 1)) + min;
-//          if (!bombs.includes(randomInt)){
-//              flag = true;
-//          }
-//      }
-//      return randomInt
-//  }
-
-// // click del pulstante diventa rosso ecc, nel secondo click
-
-// if(bombs.incudes(parseInt(this.innerText))){
-
-// }
